8b41e994c6d43a5943392aa4daf7be8871af1ccb
Javier Carey <javier3@gmail.com>
2018-10-03 13:58:28 -0700

    feat: added navigator page and added basic styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Routes from './routes'
+import Navbar from './navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar />
       <Routes />
       </div>
     );
