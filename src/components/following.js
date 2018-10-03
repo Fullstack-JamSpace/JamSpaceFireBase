6a3487994bf5298b09d3ad7d5baecb663604d746
jav1jav <javier3@gmail.com>
2018-10-03 16:14:23 -0700

    Update following.js
    superficial change to see if I can fix a pull issue I'm having locally

--- src/components/following.js
+++ src/components/following.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import db from '../firebase';
 import * as firebase from 'firebase';
+//superficial change to see if I can fix a pull issue I'm having locally
 
 
 export default class Following extends Component {
