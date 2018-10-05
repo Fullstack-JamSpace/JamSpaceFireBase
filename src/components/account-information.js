fb00d9e6b2c4427dd8b0b958bf9fb1626617e15f
Javier Carey <javier3@gmail.com>
2018-10-05 14:52:38 -0700

    feat: AccountInfo component uses same form (signup-form.js) as SignUp component, and I put the firebase.auth().onAuthStateChanged on the App component so that components below that could depend on firebase.auth().getCurrentUser. And on top of that I added a utils.js file with a 'getCurrentUser' function that will pull the user object from the firebase store (not the firebase auth tore, the regular firebase store)

--- src/components/account-information.js
+++ src/components/account-information.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import {
-  Form,
-  List,
-  Container,
-  Segment,
-  Button,
-  Divider,
-  Input,
-  Icon
-} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import SignupForm from './signup-form';
-import * as firebase from 'firebase';
+import { getCurrentUser } from '../utils'
+
 
 class AccountInfo extends React.Component {
   state = {};
-  handleClick = e => this.setState({ item: e.target.innerText });
+
   handleSubmit = event => {
-  const email = event.target.email.value;
-  //const password = event.target.password.value;
-  const firstName = event.target.firstName.value;
-  const lastName = event.target.lastName.value;
-  const displayName = event.target.displayName.value;
-  const imageUrl = event.target.imageUrl.value;
-  console.log('what!!!!', email, firstName, lastName, displayName)
+    const email = event.target.email.value;
+    //const password = event.target.password.value;
+    const firstName = event.target.firstName.value;
+    const lastName = event.target.lastName.value;
+    const displayName = event.target.displayName.value;
+    const imageUrl = event.target.imageUrl.value;
+    console.log('account-information.js | event values: ', email, firstName, lastName, displayName, imageUrl)
+  }
+
+  async componentDidMount(){
+    const jammer = await getCurrentUser()
+    this.setState({
+      jammer: jammer
+    })
   }
 
   render() {
-    let currUser;
-    firebase.auth().onAuthStateChanged( user => user ? currUser=firebase.auth().currentUser : false)
+    console.log('account-information.js | this.state', this.state)
     return (
-      <SignupForm handleSubmit={this.handleSubmit} user={currUser}/>
+
+      <Container width={7}>
+      <SignupForm handleSubmit={this.handleSubmit} user={this.state.jammer} />
+      </Container>
     )
-
-
   }
 }
 export default AccountInfo;
--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -3,31 +3,11 @@ import { Form, Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 const SignupForm = props => {
-  // <form className="submit-form" onSubmit={props.handleSignup}>
-  //   <div>
-  //     <label htmlFor="firstName">First Name</label>
-  //     <input type="text" name="firstName" required />
-  //     <br />
-  //     <label htmlFor="lastName">Last Name</label>
-  //     <input type="text" name="lastName" required />
-  //     <br />
-  //     <label htmlFor="email">Email Address</label>
-  //     <input type="email" name="email" required />
-  //     <br />
-  //     <label htmlFor="password">Password</label>
-  //     <input type="text" minLength='6' name="password" required />
-  //     <br />
-  //     <label htmlFor="displayName">Display Name</label>
-  //     <input type="text" name="displayName" required />
-  //     <br />
-  //     <label htmlFor="imageUrl">Profile Picture</label>
-  //     <input type="text" name="imageUrl" />
-  //     <br />
-  //     <button type="submit">Submit!</button>
-  //   </div>
-  // </form>
 
   const lableWidth = 6
+
+  // If AccountForm component isn't passed a user, initialize values to show
+  // in the form
   const user = props.user ?
     props.user :
     {
@@ -51,7 +31,7 @@ const SignupForm = props => {
           placeholder={user.email} width={lableWidth} required />
 
           <Form.Input name="password" label="Password"
-          placholder={user.password} width={lableWidth} minLength="6" required />
+          placeholder={user.password} width={lableWidth} minLength="6" required />
 
           <Form.Input name="displayName" label="Display name"
           placeholder={user.displayName} width={lableWidth} />
