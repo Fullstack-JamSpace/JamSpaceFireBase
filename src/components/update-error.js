76220076f2ff84a0c51471cb25f72b4309f0e3ba
Javier Carey <javier3@gmail.com>
2018-10-06 22:14:29 -0700

    feat & bugfix: Edit Account info flow has been updated to perform well - redirects properly to update-success and update-error pages in those events and those pages have links to send you back to the account profile page. Email and password fields are disabled when editing the profile. We can handle that flow later. There is still a warning in the console regarding password field being controlled then uncontrolled that I'd like to fix, but not tonight

--- src/components/update-error.js
+++ src/components/update-error.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 
-export const UpdateSuccessful = (props) => (
+export const UpdateError = (props) => (
   <div>
-    <h1 >Yay!</h1>
-    <h5>Update completed successfully!</h5>
+    <h1 >Bummer...</h1>
+    <h5>Error completing update.</h5>
     <Button onClick={props.handleClick}>Return to account information</Button>
   </div>
 )
