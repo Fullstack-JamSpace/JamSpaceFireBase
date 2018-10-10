bb95295adb407e361fff20704cff2d1b98b7438f
Javier Carey <javier3@gmail.com>
2018-10-10 16:51:28 -0700

    bugfix: updated stream-nav to use HOC to be hooked to user snapshot, which fixed rendering issue when follow button was clicked

--- src/components/follow-button.js
+++ src/components/follow-button.js
@@ -1,77 +1,78 @@
-import React, {Component} from 'react';
+import React from 'react';
 import * as firebase from 'firebase';
 import db from '../firebase';
-import '../css/follow-button.css'
+import '../css/follow-button.css';
 import { Button } from 'semantic-ui-react';
-import { getCurrentUser } from '../utils';
+//import { getCurrentUser } from '../utils';
 
 //if following contains displayName render disabled button
 
-export default class FollowButton extends Component {
-  constructor() {
-    super()
-    this.state = {
-      user: {},
-      streamer: {},
-      isFollowing: false
-    }
-  }
+export const FollowButton = (props) => {
 
-  async componentDidMount() {
-    let streamer = {};
+  const user = props.user;
+  const following = user.following;
+
+  const streamer = props.streamer;
+  let followers = streamer.followers || 0;
+  // if (!followers) followers = 0;
+
+  let isFollowing = following && following.includes(streamer.displayName);
+
+  const handleClick = async () => {
     try {
-      const user = await getCurrentUser()
-      const following = user.following;
-      if (following && following.includes(this.props.displayName)) {
-        this.setState({isFollowing: true})
-      }
-      const streamerRef = await db.collection('jammers').where('displayName', '==', `${this.props.displayName}`).get()
-      streamerRef.forEach(doc => streamer = doc.data())
+      const userData = await db.collection('jammers')
+        .doc(`${user.email}`);
 
-      this.setState({ user, streamer })
-    } catch (error) {
-      console.log(error);
-    }
-  }
+      const streamerData = await db.collection('jammers')
+        .doc(`${streamer.email}`);
 
-  handleClick = async () => {
-    const { user, isFollowing, streamer } = this.state
-    let followers = streamer.followers
-    if(!followers) followers = 0
-    try {
-      const userData = await db.collection('jammers').doc(`${user.email}`)
-      const streamerData = await db.collection('jammers').doc(`${streamer.email}`)
-      if(!isFollowing) {
-        await userData.update({...user,
-          following: firebase.firestore.FieldValue.arrayUnion(`${streamer.displayName}`),
-        })
-        await streamerData.update({...streamer,
-          followers: followers += 1
-        })
-
+      if (!isFollowing) {
+        await userData.update({
+          ...user,
+          following: firebase.firestore.FieldValue.arrayUnion(
+            `${streamer.displayName}`
+          )
+        });
+        await streamerData.update({
+          ...streamer,
+          followers: (followers += 1)
+        });
       } else {
-        await userData.update({...user,
-          following: firebase.firestore.FieldValue.arrayRemove(`${streamer.displayName}`)
-        })
-        if(followers > 0) {
-          await streamerData.update({...streamer,
-            followers: followers -= 1
-          })
+        await userData.update({
+          ...user,
+          following: firebase.firestore.FieldValue.arrayRemove(
+            `${streamer.displayName}`
+          )
+        });
+        if (followers > 0) {
+          await streamerData.update({
+            ...streamer,
+            followers: (followers -= 1)
+          });
         }
       }
 
-      this.setState({ isFollowing : !isFollowing })
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  render() {
-    const { isFollowing } = this.state
-    return (
-      !isFollowing
-      ? <Button className='follow-button' onClick={this.handleClick} icon='user' content='Follow' />
-      : <Button positive className='follow-button' onClick={this.handleClick} icon='checkmark' content='Following'/>
-    )
-  }
+
+
+  return !isFollowing ? (
+    <Button
+      className="follow-button"
+      onClick={handleClick}
+      icon="user"
+      content={`${streamer.displayName}`}
+    />
+  ) : (
+    <Button
+      positive
+      className="follow-button"
+      onClick={handleClick}
+      icon="checkmark"
+      content={`${streamer.displayName}`}
+    />
+  );
 };
