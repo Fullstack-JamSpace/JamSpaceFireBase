6ef853f43d67cd3a8bb20af9c32f99054ade3a54
Javier Carey <javier3@gmail.com>
2018-10-16 14:46:02 -0700

    bugfix: used a bit of a hack to fix it so that logout would re-render the navbar - basically took a function that we were alredy using in the navbar to pass down to menu items to handle click and passed it to the logout menu item. clicking that would kick off 'handle click' which would change state on the navbar and voila. I don't know how I'll sleep at night, but it works

--- src/components/navbar/profile-icon.js
+++ src/components/navbar/profile-icon.js
@@ -1,62 +1,71 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { Dropdown } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react';
 import '../../css/navbar.css';
-import * as firebase from 'firebase'
+import * as firebase from 'firebase';
 
-export class ProfileIcon extends Component{
+export const ProfileIcon = props => {
 
-  handleLogout = () => {
-    firebase.auth().signOut()
-  }
+  this.handleItemClick =
+    props.handleItemClick
 
-  render(){
-    const { user } = this.props;
-    const trigger = (
-      <span>
-        <p id='drop-text'>
-          Hello, {user.displayName}
-        </p>
-      </span>
-    )
+  this.handleLogout = () => {
+    firebase.auth().signOut();
+    this.handleItemClick()
+  };
 
-    const options = [
-      {
-        key: 'user',
-        text: (
-          <span>
-            Signed in as <strong>{user.displayName}</strong>
-          </span>
-        ),
-        disabled: true,
-      },
-      { key: 'profile',
-        text: (
-          <Link className='no-deco list-item' to={`/channels/${user.displayName}`}>
-            <p className='drop-choice'>Stream</p>
-          </Link>
-        )
-      },
-      { key: 'settings', text: (
-        <Link className='no-deco' to={`/channels/${user.displayName}/account`}>
-          <p className='drop-choice'>Account Settings</p>
+
+
+  const { user } = props;
+  const trigger = (
+    <span>
+      <p id="drop-text">Hello, {user.displayName}</p>
+    </span>
+  );
+
+  const options = [
+    {
+      key: 'user',
+      text: (
+        <span>
+          Signed in as <strong>{user.displayName}</strong>
+        </span>
+      ),
+      disabled: true
+    },
+    {
+      key: 'profile',
+      text: (
+        <Link className="no-deco list-item" to={`/channels/${user.displayName}`}>
+          <p className="drop-choice">Stream</p>
         </Link>
-      ) },
-      { key: 'sign-out', text: (
-        <Link className='no-deco' to='/' onClick={this.handleLogout}>
-          <p className='drop-choice'>Sign Out</p>
+      )
+    },
+    {
+      key: 'settings',
+      text: (
+        <Link className="no-deco" to={`/channels/${user.displayName}/account`}>
+          <p className="drop-choice">Account Settings</p>
         </Link>
-      ) },
-    ]
+      )
+    },
+    {
+      key: 'sign-out',
+      name: 'Browse by Category', //hacky way to get logout to re-render nav
+      text: (
+        <Link className="no-deco" to="/" onClick={this.handleLogout}>
+          <p className="drop-choice">Sign Out</p>
+        </Link>
+      )
+    }
+  ];
 
-    return (
-      <Fragment>
-      <div id='user-drop-down'>
-        <img id='user-sprite' src={user.imageUrl} alt='user-pic'/>
-          <Dropdown trigger={trigger} options={options} />
+  return (
+    <Fragment>
+      <div id="user-drop-down">
+        <img id="user-sprite" src={user.imageUrl} alt="user-pic" />
+        <Dropdown trigger={trigger} options={options} />
       </div>
-      </Fragment>
-    )
-  }
-
-}
+    </Fragment>
+  );
+};
