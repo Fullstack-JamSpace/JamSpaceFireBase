2232714b765a096e198e201affc0a8dcd968b483
James Byrd <James.Byrd1265@gmail.com>
2019-02-26 16:28:03 -0500

    fix: scrollToTop root level

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './app';
 import registerServiceWorker from './registerServiceWorker';
 import { Router } from 'react-router-dom';
 import history from './history';
+import { Switch, Route } from 'react-router-dom';
+import ScrollToTop from './scroll-to-top'
 
 ReactDOM.render(
   <Router history={history}>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </Router>
 , document.getElementById('root'));
 registerServiceWorker();
